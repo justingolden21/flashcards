55035168452bb0ecdca2757a000b2960712f7545
Justin Golden <justingolden21@gmail.com>
2020-03-12 10:04:29 -0700

    init

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+let app1 = angular.module('app1', []);
+
+app1.controller('ctrl1', ($scope)=> {
+
+	log = ()=> {
+		for($card of $scope.cards)
+			console.log($card);
+	}
+
+	$scope.remove = (card)=> {
+		$scope.cards = $scope.cards.filter(obj=> obj.front!=card.front || obj.back!=card.back);
+	}
+
+	$scope.new = ()=> {
+		$scope.cards.push({front: '', back: '', flipped: false});
+	}
+
+	$scope.removeAll = ()=> {
+		$scope.cards = [];
+	}
+
+});
+
+$( ()=> {
+	$('#flashcards').sortable();
+});
